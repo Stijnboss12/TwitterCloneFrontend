6f558882ed50e59e436c6c9be072f9b67bfc8d25
Stijn <434876@student.fontys.nl>
2022-03-24 12:55:08 +0100

    setup new project and add authentication

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+
+      <router-link v-if="$auth.isAuthenticated" to="/profile"
+        >Profile</router-link
+      >
     </nav>
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { authGuard } from '../auth/authguard';
 
 Vue.use(VueRouter)
 
@@ -8,16 +8,19 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: () => import('../views/HomeView.vue')
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => import('../views/ProfileView.vue'),
+    beforeEnter: authGuard
+  },
 ]
 
 const router = new VueRouter({
