49d6a20955685dacf14875fb880af5452308aec4
Stijn <434876@student.fontys.nl>
2022-03-25 12:39:09 +0100

    new navbar

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,59 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-
-      <router-link v-if="$auth.isAuthenticated" to="/profile"
-        >Profile</router-link
-      >
-    </nav>
-    <router-view/>
+    <body style="background-color: #012549">
+      <nav class="navbar fixed-bottom navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">TwitterClone</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="container-fluid">
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+              <ul class="navbar-nav mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a v-if="$auth.isAuthenticated" class="nav-link" href="/profile">Profile</a>
+                </li>
+                <li class="nav-item">
+                  <a v-if="!$auth.isAuthenticated" @click="login" class="btn btn-success">Log in</a>
+                </li>
+                <li class="nav-item">
+                  <a v-if="$auth.isAuthenticated" @click="logout" class="btn btn-danger">Log out</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <router-view />
+    </body>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import "./assets/App.scss";
 </style>
+
+<script>
+export default {
+  methods: {
+    // Log the user in
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    // Log the user out
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    }
+  }
+};
+</script>
