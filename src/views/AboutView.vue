6155da4efada7e2de4b63d3e520d8d5280a8e982
Stijn <434876@student.fontys.nl>
2022-03-25 12:39:52 +0100

    homepage setup

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,50 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container-fluid text-white">
+    <div class="row">
+      <div class="col">
+        <a class="navbar-brand text-white" href="/" style="font-size: 300%;"
+          >TwitterClone</a
+        >
+      </div>
+      <div class="col-6">
+          <form class="d-flex m-md-2">
+            <input
+              class="form-control me-2 bg-dark border-dark text-white"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        <div class="card m-md-2" style="background-color: #011a32;">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </div>
+        </div>
+        <div class="card m-md-2" style="background-color: #011a32;">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </div>
+        </div>
+        <div class="card m-md-2" style="background-color: #011a32;">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col">menno</div>
+    </div>
   </div>
 </template>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,9 +6,9 @@
     <!-- Check that the SDK client is not currently loading before accessing is methods -->
     <div v-if="!$auth.loading">
       <!-- show login when not authenticated -->
-      <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+      <button v-if="!$auth.isAuthenticated" @click="login" class="btn btn-primary">Log in</button>
       <!-- show logout when authenticated -->
-      <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+      <button v-if="$auth.isAuthenticated" @click="logout" class="btn btn-danger">Log out</button>
     </div>
   </div>
 </template>
