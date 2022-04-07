50dad3b9cb6be89c0fe324942ebf97576c84c02c
Stijn <434876@student.fontys.nl>
2022-04-07 12:29:25 +0200

    setup home page to retrieve posts upon load, ability to create new posts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,59 +7,87 @@
         >
       </div>
       <div class="col-6">
-          <form class="d-flex m-md-2 row">
-            <b-textarea
-              class="form-control mb-2 border-dark"
-              type="textarea"
-              placeholder="Type your message here!"
-              aria-label="message"
-              maxlength="300"
-              rows="4"
-              no-resize
-              style="background-color: #011a32; color: #d4d4d4;"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Post
-            </button>
-          </form>
-        <div class="card m-md-2" style="background-color: #011a32;">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </p>
-          </div>
-        </div>
-        <div class="card m-md-2" style="background-color: #011a32;">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </p>
-          </div>
-        </div>
-        <div class="card m-md-2" style="background-color: #011a32;">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </p>
+        <form class="d-flex m-md-2 row" @submit.prevent="CreatePost">
+          <b-textarea
+            class="form-control mb-2 border-dark"
+            type="textarea"
+            placeholder="Type your message here!"
+            aria-label="message"
+            maxlength="300"
+            rows="4"
+            v-model="Content"
+            no-resize
+            style="background-color: #011a32; color: #d4d4d4"
+          />
+          <button class="btn btn-outline-success" type="submit">Post</button>
+        </form>
+        <div v-for="post in Posts" :key="post.id">
+          <div class="card m-md-2" style="background-color: #011a32;">
+            <div class="card-body">
+              <h5 class="card-title">{{ post.userId }}</h5>
+              <p class="card-text">
+                {{ post.content }}
+              </p>
+            </div>
           </div>
         </div>
       </div>
       <div class="col">
-          <form class="d-flex m-md-2">
-            <input
-              class="form-control me-2 bg-dark border-dark text-white"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
+        <form class="d-flex m-md-2">
+          <input
+            class="form-control me-2 bg-dark border-dark text-white"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      Posts: [],
+      Content: "",
+    };
+  },
+  async created() {
+    const token = await this.$auth.getTokenSilently();
+
+    axios
+      .get(`${process.env.VUE_APP_API_BASEURL}Posts`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        if (response.status == 200) {
+          this.Posts = response.data;
+        }
+      });
+  },
+  methods: {
+    async CreatePost() {
+      // Get the access token from the auth wrapper
+      const token = await this.$auth.getTokenSilently();
+
+      // Use Axios to make a call to the API
+      this.object = await axios.post(
+        `${process.env.VUE_APP_API_BASEURL}Posts/New`,
+        { Content: this.Content },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      window.location.reload();
+    },
+  },
+};
+</script>
