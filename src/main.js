4a1291027ef78ea7c29a07bdf7318d1024d7d2cc
Stijn <434876@student.fontys.nl>
2022-04-15 14:19:03 +0200

    add vue luxon to mainjs with setup

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueLuxon from "vue-luxon";
 
 // Import the Auth0 configuration
 import { domain, clientId, audience } from '../auth_config.json';
@@ -26,6 +27,21 @@ Vue.use(Auth0Plugin, {
 });
 
 Vue.use(BootstrapVue)
+
+Vue.use(VueLuxon, {
+  input: {
+      zone: "utc",
+      format: "iso"
+  },
+  output: {
+    format: {
+      weekday: 'long',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    }
+  }
+});
 
 Vue.config.productionTip = false
 
